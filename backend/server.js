8caab1c748fc866c44f11a9957e2f3735b5c6254
sajithgandhi <sajzsun@gmail.com>
2023-12-27 08:53:48 +0100

    Get products from database

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 const port = process.env.PORT || 3002;
 
@@ -15,14 +15,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
