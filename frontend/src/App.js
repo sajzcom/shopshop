d0a15b41b06e50c9de795e459d45ce5fa8c6cb14
sajzcom <sajzcom@gmail.com>
2024-07-05 16:29:34 +0530

    Change header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Shopshop</h1>
+          <h1>Welcome to GitShop</h1>
           <Outlet />
         </Container>
       </main>
