27ca80e2661c5b99c4387506539cfe5b40a8ffbc
sajzcom <mindflow@sajz.com>
2024-07-10 19:52:42 +0530

    compshop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to shopshop</h1>
+          <h1>Welcome to compshop</h1>
           <Outlet />
         </Container>
       </main>
